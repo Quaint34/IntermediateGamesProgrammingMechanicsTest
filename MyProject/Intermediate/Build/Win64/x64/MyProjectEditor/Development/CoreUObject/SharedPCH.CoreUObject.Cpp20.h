f128e1788e4da9de8587f75b58de71674153d9c9
Quaint34 <75154764+Quaint34@users.noreply.github.com>
2026-05-24 17:28:34 +1000

    fixed jump pads + audio

--- MyProject/Intermediate/Build/Win64/x64/MyProjectEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
+++ MyProject/Intermediate/Build/Win64/x64/MyProjectEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/Users/s5433360/Documents/GitHub/IntermediateGamesProgrammingMechanicsTest/MyProject/Intermediate/Build/Win64/x64/MyProjectEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
+#include "C:/Users/Zandir/Documents/GitHub/IntermediateGamesProgrammingMechanicsTest/MyProject/Intermediate/Build/Win64/x64/MyProjectEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
 #include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
--- MyProject/Intermediate/Build/Win64/x64/MyProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.cpp
+++ MyProject/Intermediate/Build/Win64/x64/MyProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.38.33145
-#include "C:/Users/s5433360/Documents/GitHub/IntermediateGamesProgrammingMechanicsTest/MyProject/Intermediate/Build/Win64/x64/MyProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h"
+#include "C:/Users/Zandir/Documents/GitHub/IntermediateGamesProgrammingMechanicsTest/MyProject/Intermediate/Build/Win64/x64/MyProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h"
